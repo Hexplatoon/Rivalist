668784cfba2531e84f014efa6bd6677f96015407
Sahilpatel2507 <sahilpatel2507@gmail.com>
2025-03-23 22:18:35 +0530

    typing module testing

---
+++ typingbattleapplication/src/main/java/com/hexplatoon/typingbattleapplication/TypingbattleapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.hexplatoon.typingbattleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingbattleapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingbattleapplicationApplication.class, args);
+	}
+
+}
