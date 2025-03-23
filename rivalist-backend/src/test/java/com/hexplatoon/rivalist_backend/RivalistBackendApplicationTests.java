29df10249f13c38220fb59f027701cac0a909b00
hexwhiz <iamsanyamgoel@gmail.com>
2025-03-23 22:00:05 +0530

    setup commit

---
+++ rivalist-backend/src/test/java/com/hexplatoon/rivalist_backend/RivalistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexplatoon.rivalist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RivalistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
